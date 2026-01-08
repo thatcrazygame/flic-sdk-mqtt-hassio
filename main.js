f28dee7e69c26fd9b3e015f95dd010560cdd865e
thatcrazygame <thatcrazygame@gmail.com>
2026-01-08 13:21:08 -0500

    Flicapp support; Duo support

--- main.js
+++ main.js
@@ -15,36 +15,40 @@ const FW_INTERVAL = 30000;
 const MQTT = require("./mqtt").create(SERVER, {username: USERNAME, password: PASSWORD});
 const HUB = require("hubinfo");
 const BUTTON_MANAGER = require("buttons");
+const FLICAPP = require("flicapp");
 // Device info
 const SERIAL = HUB.serialNumber.toLowerCase();
 const HUB_MODEL = "Flic Hub LR";
 const FLICHUB = "flichub";
 const MANUFACTURER = "Shortcut Labs AB";
 const FLIC = "Flic";
-const TWIST = "Twist"
+const TWIST = "Twist";
+const DUO = "Duo";
 // MQTT Topics
 const HASS = "homeassistant";
 const DEVICE_AUTOMATION = `${HASS}/device_automation`;
 const SENSOR = `${HASS}/sensor`;
 const HASSIO_STATUS = `${HASS}/status`;
 const CONF = "config";
-const CLICK_TYPE = "clickType";
+const ACTION = "action";
 const FIRMWARE = "firmware";
 const DELETE_MSG = "";
+const VD_UPDATE = "virtualDeviceUpdate";
+const VD_STATE = `${VD_UPDATE}/state`;
 // Actions
 const CLICK = "click";
 const DOUBLE_CLICK = "double_click";
 const HOLD = "hold";
 const UP = "up";
 const DOWN = "down";
-// [<type>,<icon>]
-// icons for possibly setting up HA entities, not currently used
-const CLICKTYPES = [
-	[CLICK,"mdi:gesture-tap"],
-	[DOUBLE_CLICK, "mdi:gesture-double-tap"],
-	[HOLD, "mdi:gesture-tap-hold"],
-	[UP, "mdi:arrow-collapse-up"],
-	[DOWN, "mdi:arrow-collapse-down"]
+const MESSAGE = "message";
+const ACTIONS = [
+	  CLICK
+	, DOUBLE_CLICK
+	, HOLD
+	, UP
+	, DOWN
+	, VD_UPDATE
 ];
 
 
@@ -62,6 +66,7 @@ function publishHubConfig() {
 	config.name = "Firmware Version";
 	config.device = device;
 	config.state_topic = `${FLICHUB}/${SERIAL}/${FIRMWARE}`;
+	config.expire_after = FW_INTERVAL / 1000 * 6;
 	
 	let topic = `${SENSOR}/${FLICHUB}_${SERIAL}/${CONF}`;
 	MQTT.publish(topic, JSON.stringify(config), {retain: true});	
@@ -87,11 +92,15 @@ function publishButtonTriggers(button) {
 	device.manufacturer = MANUFACTURER;
 
 	let device_type = FLIC;
-	if (button.flicVersion == 3) {
+	let serial = button.serialNumber;
+	if (serial.startsWith("BF39")) {
+		device.model = FLIC + " " + button.flicVersion;
+	} else if (serial.startsWith("CA22")) {
 		device_type = TWIST;
 		device.model = TWIST;
-	} else {
-		device.model = FLIC + " " + button.flicVersion;
+	} else if (serial.startsWith("DA45")) {
+		device_type = DUO;
+		device.model = DUO;
 	}
 	
 	if (button.name == null || button.name == "") {
@@ -102,18 +111,23 @@ function publishButtonTriggers(button) {
 	device.sw_version = button.firmwareVersion;
 	device.via_device = SERIAL;
 
-	for (let i = 0; i < CLICKTYPES.length; i++) {
-		let clickType = CLICKTYPES[i][0];
+	for (let i = 0; i < ACTIONS.length; i++) {
+		let action = ACTIONS[i];
 		let config = {};
 		config.automation_type = "trigger";
 		config.type = "action";
-		config.subtype = clickType;
-		config.payload = clickType;
-		config.topic = `${DEVICE_AUTOMATION}/${object_id}/${CLICK_TYPE}`;
+		config.subtype = action;
+		config.payload = action;
+		config.topic = `${DEVICE_AUTOMATION}/${object_id}/${ACTION}`;
 		config.device = device;
 		
-		if (device_type == FLIC || (device_type == TWIST && clickType != HOLD)) {
-			let topic = `${DEVICE_AUTOMATION}/${object_id}/${clickType}/${CONF}`;
+		if (action == VD_UPDATE || action == MESSAGE) {
+			delete config.payload;
+			config.topic = `${DEVICE_AUTOMATION}/${object_id}/${action}`;
+		}
+		
+		if (device_type == FLIC || (device_type == TWIST && action != HOLD) || device_type == DUO) {
+			let topic = `${DEVICE_AUTOMATION}/${object_id}/${action}/${CONF}`;
 			MQTT.publish(topic, JSON.stringify(config), {retain: true});			
 		}
 	}
@@ -128,9 +142,9 @@ function publishAllButtonTriggers() {
 
 function deleteButtonTriggers(bdaddr) {
 	let object_id = getObjectID(bdaddr);
-	for (let i = 0; i < CLICKTYPES.length; i++) {
-		let clickType = CLICKTYPES[i][0];
-		let topic = `${DEVICE_AUTOMATION}/${object_id}/${clickType}/${CONF}`;
+	for (let i = 0; i < ACTIONS.length; i++) {
+		let action = ACTIONS[i][0];
+		let topic = `${DEVICE_AUTOMATION}/${object_id}/${action}/${CONF}`;
 		MQTT.publish(topic, DELETE_MSG, {retain: true});
 	}
 }
@@ -147,28 +161,50 @@ function updateAll() {
 
 BUTTON_MANAGER.on("buttonSingleOrDoubleClickOrHold", function(obj) {
 	let object_id = getObjectID(obj.bdaddr);
-	let clickType = obj.isSingleClick ? CLICK : obj.isDoubleClick ? DOUBLE_CLICK : HOLD;
-	MQTT.publish(`${DEVICE_AUTOMATION}/${object_id}/${CLICK_TYPE}`, clickType);
+	let action = obj.isSingleClick ? CLICK : obj.isDoubleClick ? DOUBLE_CLICK : HOLD;
+	MQTT.publish(`${DEVICE_AUTOMATION}/${object_id}/${ACTION}`, action);
 });
 
 BUTTON_MANAGER.on("buttonDown", function(obj) {
 	let object_id = getObjectID(obj.bdaddr);
-	MQTT.publish(`${DEVICE_AUTOMATION}/${object_id}/${CLICK_TYPE}`, DOWN);
+	MQTT.publish(`${DEVICE_AUTOMATION}/${object_id}/${ACTION}`, DOWN);
 });
 
 BUTTON_MANAGER.on("buttonUp", function(obj) {
 	let object_id = getObjectID(obj.bdaddr);
-	MQTT.publish(`${DEVICE_AUTOMATION}/${object_id}/${CLICK_TYPE}`, UP);
+	MQTT.publish(`${DEVICE_AUTOMATION}/${object_id}/${ACTION}`, UP);
+});
+
+FLICAPP.on("actionMessage", function(message) {
+	//console.log(message);
+	let re = /^([/].+?):([^:]+?)$/;
+	let found = message.match(re);
+	if (found != null) {
+		let topic = found[1];
+		message = found[2];
+		MQTT.publish(`${FLIC}${topic}`, message);	
+	} else {
+		MQTT.publish(`${FLIC}/action_message`, message);	
+	}	
+});
+
+FLICAPP.on(VD_UPDATE, function(metaData, values) {
+	let data = {};
+	data.meta_data = metaData;
+	data.values = values;
+	let object_id = getObjectID(metaData.buttonId);
+	// console.log(JSON.stringify(data));
+	MQTT.publish(`${DEVICE_AUTOMATION}/${object_id}/${VD_UPDATE}`, JSON.stringify(data));
 });
 
 BUTTON_MANAGER.on("buttonReady", function(obj) {
-	console.log("ready");
+	// console.log("ready");
 	let button = BUTTON_MANAGER.getButton(obj.bdaddr);
 	publishButtonTriggers(button);
 });
 
 BUTTON_MANAGER.on("buttonUpdated", function(obj) {
-	console.log("updated");
+	// console.log("updated");
 	let button = BUTTON_MANAGER.getButton(obj.bdaddr);
 	if (button.name != null && button.name != "") {
 		publishButtonTriggers(button);
@@ -195,6 +231,7 @@ MQTT.on("error", function(message) {
 
 MQTT.on("connected", function() {
 	MQTT.subscribe(HASSIO_STATUS);
+	MQTT.subscribe(`${FLIC}/${VD_STATE}`);
 	updateAll();
 });
 
@@ -202,6 +239,19 @@ MQTT.on("publish", function(pub) {
 	// HA publishes a message when it (re)connects to the MQTT broker
 	if (pub.topic == HASSIO_STATUS && pub.message == "online") {
 		updateAll();
+	}
+	if (pub.topic == `${FLIC}/${VD_STATE}`) {
+		let update = {};
+		try {
+			update = JSON.parse(pub.message);
+		} catch (error) {
+			console.log(`Error parsing ${pub.message}`)
+			console.log(error);
+		}
+		if ("type" in update && "id" in update && "values" in update) {
+			// console.log(update.type, update.id, JSON.stringify(update.values));
+			FLICAPP.virtualDeviceUpdateState(update.type, update.id, update.values);
+		}
 	}
 });
 
